37343d6929c76b9ec94a033e26048e262999fbbf
UttaraPraveen <uttarapraveen2005@gmail.com>
2026-06-29 17:36:10 +0530

    fix

--- src/pages/Letters.tsx
+++ src/pages/Letters.tsx
@@ -4,27 +4,24 @@ import "../styles/letters.css";
 
 function Letters() {
   return (
-    <main className="letters-page">
-      <h1>Letters for Every Age</h1>
+    import PageLayout from "../components/PageLayout";
 
-      <p>
-        Some words are meant for the person you are today.
-        Others are waiting patiently for the person you'll
-        become.
-      </p>
-
-      <div className="letters-grid">
-        {letters.map((letter) => (
-          <LetterCard
-            key={letter.id}
-            header={`Age ${letter.age}`}
-            title={letter.title}
-            preview={letter.preview}
-            content={letter.content}
-          />
-        ))}
-      </div>
-    </main>
+<PageLayout
+  title="Letters for Every Age"
+  subtitle="Some words are meant for the person you are today. Others are waiting patiently for the person you'll become."
+>
+  <div className="letters-grid">
+    {letters.map((letter) => (
+      <LetterCard
+        key={letter.id}
+        header={`Age ${letter.age}`}
+        title={letter.title}
+        preview={letter.preview}
+        content={letter.content}
+      />
+    ))}
+  </div>
+</PageLayout>
   );
 }
 
--- src/pages/OpenWhen.tsx
+++ src/pages/OpenWhen.tsx
@@ -1,5 +1,33 @@
+import LetterCard from "../components/LetterCard";
+import { openWhenLetters } from "../data/openWhen";
+
+import "../styles/letters.css";
+
 function OpenWhen() {
-  return <h1>Open When</h1>;
+  return (
+    <main className="letters-page">
+      <h1>Open When...</h1>
+
+      <p>
+        Some words are meant for moments
+        you can't prepare for.
+        <br />
+        Take whichever one you need today.
+      </p>
+
+      <div className="letters-grid">
+        {openWhenLetters.map((letter) => (
+          <LetterCard
+            key={letter.id}
+            header="Sealed Letter"
+            title={letter.title}
+            preview={letter.preview}
+            content={letter.content}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default OpenWhen;
